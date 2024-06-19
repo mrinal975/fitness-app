bbb80b0c079be425be58ab95ebf071f9088b8323
Mrinal Mallik <mrinalmallik1@gmail.com>
2024-06-19 21:05:34 +0600

    alies added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import "./App.css";
-
+import About from "src/Pages/About";
 function App() {
   return (
     <>
+      <About />
       <div className="text-4xl font-bold text-red-500">Hello</div>
     </>
   );
