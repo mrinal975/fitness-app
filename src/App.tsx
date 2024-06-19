b710428812406cb547dce82dd41348d1c73bcc88
Mrinal Mallik <mrinalmallik1@gmail.com>
2024-06-19 21:56:12 +0600

    tailwind config add

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import "./App.css";
 import About from "src/Pages/About";
 function App() {
   return (
-    <>
+    <div className="app">
       <About />
       <div className="text-4xl font-bold text-red-500">Hello</div>
-    </>
+    </div>
   );
 }
 
