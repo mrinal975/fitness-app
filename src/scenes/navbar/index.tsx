837d29f2993e72cfa9c660e96af402f9c18edb39
Mrinal Mallik <mrinalmallik1@gmail.com>
2024-06-20 12:26:12 +0600

    react-anchor-link-smooth-scrool declear type

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -2,6 +2,9 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import { SelectedPage } from "src/shared/types";
 import Logo from "src/assets/Logo.png";
 import Link from "./Link";
+import useMediaQuery from "src/hooks/useMediaQuery";
+import { useState } from "react";
+import ActionButton from "src/shared/ActionButton";
 
 type Props = {
   selectedPage: SelectedPage;
@@ -10,6 +13,8 @@ type Props = {
 
 function Navbar({ selectedPage, setSelectedPage }: Props) {
   const flexBetween = "flex items-center justify-between";
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+  const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
   return (
     <nav className="{flexBetween} fixed top-0 z-30 w-full py-6">
       <div className={`${flexBetween} mx-auto w-5/6 `}>
@@ -18,35 +23,46 @@ function Navbar({ selectedPage, setSelectedPage }: Props) {
           <img alt="Logo" src={`${Logo}`} />
 
           {/* Right Side */}
-          <div className={`${flexBetween} w-full`}>
-            <div className={`${flexBetween} gap-8 text-sm`}>
-              <Link
-                page="Home"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              />{" "}
-              <Link
-                page="Banner"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              />{" "}
-              <Link
-                page="Our Classes"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              />{" "}
-              <Link
-                page="Contact Us"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              />
-              <p></p>
+          {isAboveMediumScreens ? (
+            <div className={`${flexBetween} w-full`}>
+              <div className={`${flexBetween} gap-8 text-sm`}>
+                <Link
+                  page="Home"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />{" "}
+                <Link
+                  page="Banner"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />{" "}
+                <Link
+                  page="Our Classes"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />{" "}
+                <Link
+                  page="Contact Us"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <p></p>
+              </div>
+              <div className={`${flexBetween} gap-8`}>
+                <p className="">Sign In</p>
+                <ActionButton setSelectedPage={setSelectedPage}>
+                  Become a Member
+                </ActionButton>
+              </div>
             </div>
-            <div className={`${flexBetween} gap-8`}>
-              <p className="">Sign In</p>
-              <button className=""> Become a Member</button>
-            </div>
-          </div>
+          ) : (
+            <button
+              className="rounded-full bg-secondary-500"
+              onClick={() => setIsMenuToggled(isMenuToggled)}
+            >
+              <Bars3Icon className="h-6 w-6 text-white" />
+            </button>
+          )}
         </div>
       </div>
     </nav>
