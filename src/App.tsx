f9770728341d9422c4982bb14b78e1d1effbfa11
Mrinal Mallik <mrinalmallik1@gmail.com>
2024-06-20 14:54:34 +0600

    benefit module created

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Navbar from "./scenes/navbar";
 import { SelectedPage } from "./shared/types";
 import Home from "./scenes/home";
+import Benefits from "./scenes/benefits";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState(SelectedPage.Home);
@@ -28,6 +29,7 @@ function App() {
         setSelectedPage={setSelectedPage}
       />
       <Home setSelectedPage={setSelectedPage} />
+      <Benefits setSelectedPage={setSelectedPage} />
     </div>
   );
 }
---
+++ src/scenes/benefits/index.tsx
@@ -0,0 +1,10 @@
+import { SelectedPage } from "src/shared/types";
+
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
+function Benefits({ setSelectedPage }: Props) {
+  return <div></div>;
+}
+
+export default Benefits;
